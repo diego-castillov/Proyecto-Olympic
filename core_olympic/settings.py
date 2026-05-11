0364e062f2aa2f853f04767a21380a7f8eda03c8
diego-castillov <diegoeduardocastillovillarroel@gmail.com>
2026-05-11 08:56:11 -0400

    Credenciales ocultas

--- core_olympic/settings.py
+++ core_olympic/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,16 +77,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core_olympic.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'olympic_db_v3',
-        'USER': 'root',
-        'PASSWORD': 'Di3casoff1005',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
